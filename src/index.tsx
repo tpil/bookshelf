227dc81f422002f6aff1d33f0ff7975d09cf20ed
teopyl <teopyl@gmail.com>
2023-12-10 23:37:45 +0200

    chage to HashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import { BooksProvider } from './contexts/books.context';
 
 const root = ReactDOM.createRoot(
@@ -11,11 +11,13 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
+      {/* <BrowserRouter> */}
       <BooksProvider>
         <App />
       </BooksProvider>
-    </BrowserRouter>
+      {/* </BrowserRouter> */}
+    </HashRouter>
   </React.StrictMode>
 );
 
